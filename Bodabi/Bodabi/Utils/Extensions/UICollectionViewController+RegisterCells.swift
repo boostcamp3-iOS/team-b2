a9abf4e4bce4c05be217f26f1cb3987971fe757f
Kim DongHwan <kylo2001@naver.com>
2019-01-25 15:12:32 +0900

    Add extensions for alert, action sheet and register method for table view and collection view cells.

---
+++ Bodabi/Bodabi/Utils/Extensions/UICollectionViewController+RegisterCells.swift
@@ -0,0 +1,9 @@
+//
+//  UICollectionViewController+RegisterCells.swift
+//  Bodabi
+//
+//  Created by Kim DongHwan on 25/01/2019.
+//  Copyright © 2019 LeeHyeJin. All rights reserved.
+//
+
+import Foundation
