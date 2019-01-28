74b6688bc1e4fdc681025e7b756a30f29a1a2742
nowstring <ljhhyun99@gmail.com>
2019-01-28 11:27:15 +0900

    Add setting and notification view files

---
+++ Bodabi/Bodabi/Views/Notification/NotificationViewCell.swift
@@ -0,0 +1,45 @@
+//
+//  NotificationViewCell.swift
+//  Bodabi
+//
+//  Created by jaehyeon lee on 27/01/2019.
+//  Copyright © 2019 LeeHyeJin. All rights reserved.
+//
+
+import UIKit
+
+class NotificationViewCell: UITableViewCell {
+    
+
+    @IBOutlet weak var imageContainerView: UIView!
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var eventDateLabel: UILabel!
+    @IBOutlet weak var notificationLabel: UILabel!
+    @IBOutlet weak var notificationDateLabel: UILabel!
+    
+    // MARK: - Properties
+
+    var notification: Notification? {
+        didSet {
+            guard let notification = notification else {
+                iconImageView.image = UIImage()
+                eventDateLabel.text = ""
+                notificationLabel.text = ""
+                notificationDateLabel.text = ""
+                return
+            }
+            
+            imageContainerView.makeRound(with: .heightRound)
+            // TODO: - Image Setup for each holiday
+            iconImageView.image = UIImage(named: "ic_fullStar")
+            eventDateLabel.text = notification.date
+            notificationLabel.text = notification.sentence
+            notificationDateLabel.text = "1일 전"
+        }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.notification = nil
+    }
+}
---
+++ Bodabi/Bodabi/Views/Setting/SettingViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  SettingViewCell.swift
+//  Bodabi
+//
+//  Created by jaehyeon lee on 27/01/2019.
+//  Copyright © 2019 LeeHyeJin. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    var setting: SettingOptions? {
+        didSet {
+            guard let setting = setting else {
+                textLabel?.text = ""
+                detailTextLabel?.text = ""
+                return
+            }
+            
+            switch setting {
+            case .fontSize:
+                let fontSize = UserDefaults.standard.integer(forKey: "UserFontSize")
+                detailTextLabel?.isHidden = false
+                detailTextLabel?.text = String(fontSize)
+            default:
+                detailTextLabel?.isHidden = true
+            }
+            
+            textLabel?.text = setting.description()
+        }
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.setting = nil
+    }
+}
