f89b840579112771903f14f7bac619db61a06b8f
nowstring <ljhhyun99@gmail.com>
2019-01-29 15:51:55 +0900

    Modify UIColor extension about key color

--- Bodabi/Bodabi/Utils/Extensions/UIColor+Extensions.swift
+++ Bodabi/Bodabi/Utils/Extensions/UIColor+Extensions.swift
@@ -11,22 +11,22 @@ import UIKit
 
 extension UIColor {
     class var mainColor: UIColor {
-        return #colorLiteral(red: 0.8980392157, green: 0.2588235294, blue: 0.1764705882, alpha: 1)
+        return #colorLiteral(red: 0.8588235294, green: 0.4588235294, blue: 0.368627451, alpha: 1)
     }
     
     class var weddingColor: UIColor {
-        return #colorLiteral(red: 0.9333333333, green: 0.4039215686, blue: 0.3176470588, alpha: 1)
+        return #colorLiteral(red: 0.9165593982, green: 0.6855664849, blue: 0.6328268647, alpha: 1)
     }
     
     class var starColor: UIColor {
-        return #colorLiteral(red: 1, green: 0.7723571658, blue: 0.3705800474, alpha: 1)
+        return #colorLiteral(red: 0.9792938828, green: 0.7754263282, blue: 0.4397826791, alpha: 1)
     }
     
     class var calendarColor: UIColor {
-        return #colorLiteral(red: 0.2823529412, green: 0.5803921569, blue: 0.9803921569, alpha: 1)
+        return #colorLiteral(red: 0.8689805865, green: 0.9021388292, blue: 0.9795609117, alpha: 1)
     }
     
     class var offColor: UIColor {
-        return #colorLiteral(red: 0.737254902, green: 0.7333333333, blue: 0.7450980392, alpha: 1)
+        return #colorLiteral(red: 0.9332286716, green: 0.9333921075, blue: 0.9332306385, alpha: 1)
     }
 }
