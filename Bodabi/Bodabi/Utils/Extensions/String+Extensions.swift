ab2d309a404b7b5c29e6d812dbb624636e99262b
Kim DongHwan <kylo2001@naver.com>
2019-01-30 10:32:04 +0900

    Modify string extension for insert comma

--- Bodabi/Bodabi/Utils/Extensions/String+Extensions.swift
+++ Bodabi/Bodabi/Utils/Extensions/String+Extensions.swift
@@ -49,36 +49,16 @@ extension String {
     }
     
     func insertComma() -> String? {
-        let numberFormatter = NumberFormatter(); numberFormatter.numberStyle = .decimal
+        let withSeparator: NumberFormatter = {
+            let formatter = NumberFormatter()
+            formatter.groupingSeparator = ","
+            formatter.numberStyle = .decimal
+            return formatter
+        }()
         
-        if let _ = self.range(of: ".") {
-            var numberArray = self.components(separatedBy: ".")
-            if numberArray.count == 1 {
-                var numberString = numberArray[0]
-                if numberString.isEmpty {
-                    numberString = "0"
-                }
-                guard let doubleValue = Double(numberString) else {
-                    return self
-                }
-                return numberFormatter.string(from: NSNumber(value: doubleValue)) ?? self
-            } else if numberArray.count == 2 {
-                var numberString = numberArray[0]
-                if numberString.isEmpty {
-                    numberString = "0"
-                }
-                guard let doubleValue = Double(numberString) else {
-                    return self
-                }
-                return (numberFormatter.string(from: NSNumber(value: doubleValue)) ?? numberString) + ".\(numberArray[1])"
-            }
-        } else {
-            guard let doubleValue = Double(self) else {
-                return self
-            }
-            return numberFormatter.string(from: NSNumber(value: doubleValue)) ?? self
-        }
-        return self
+        guard let number = Int(self) else { return self }
+        
+        return withSeparator.string(from: number as NSNumber)
     }
     
     func deleteComma() -> String {
