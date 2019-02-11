43152a4afa31d1dafd2f49f1054ed3860c1ee5e7
nowstring <ljhhyun99@gmail.com>
2019-02-11 14:17:37 +0900

    Modfiy core data model to add attributes

--- Bodabi/Bodabi/Models/History+Extensions.swift
+++ Bodabi/Bodabi/Models/History+Extensions.swift
@@ -16,14 +16,14 @@ extension History {
         if let item = self.item, let cash = Int(item) {
             return "\(self.friend?.name ?? "")님께 \(self.holiday?.addForSuffix() ?? "") \(cash)원을 전달했습니다"
         } else {
-        return "\(self.friend?.name ?? "")님께 \(self.holiday?.addForSuffix() ?? "") \(self.item?.addObjectSuffix() ?? "") 전달했습니다"
+            return "\(self.friend?.name ?? "")님께 \(self.holiday?.addForSuffix() ?? "") \(self.item?.addObjectSuffix() ?? "") 전달했습니다"
         }
     }
     var takeSentence: String {
         if let item = self.item, let cash = Int(item) {
             return "\(self.friend?.name ?? "")님께서 \(self.holiday?.addForSuffix() ?? "") \(cash)원을 전달해주셨습니다"
         } else {
-        return "\(self.friend?.name ?? "")님께서 \(self.holiday?.addForSuffix() ?? "") \(self.item?.addObjectSuffix() ?? "") 전달해주셨습니다"
+            return "\(self.friend?.name ?? "")님께서 \(self.holiday?.addForSuffix() ?? "") \(self.item?.addObjectSuffix() ?? "") 전달해주셨습니다"
         }
     }
 }
